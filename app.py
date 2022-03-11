5ed14883f861bb9664957d1ee670e156e9f62f35
BorntoDev <kittikorn.psk@gmail.com>
2022-03-11 16:17:50 +0700

    Added API URL Loading

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+import imp
+from urllib import response
 from flask import *
+import json
 import os
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
+url_api = "0.0.0.0"
+
+
 @app.route('/')
 def home():
-    return "hello world!!"
+    #read data api
+    url_api = "0.0.0.0"
+    response = urlopen(url_api)
+    data_json = json.loads(response.read())
+
+    return render_template('index.html',data = data_json)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))    
